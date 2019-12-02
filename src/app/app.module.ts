b16f79488a11df2ceb3e1379958af4f3105ca731
ginoLanetti <40967262+ginoLanetti@users.noreply.github.com>
2019-12-02 19:17:26 +0100

    Created basic components (#1)
    *created reusable input component
     *created file loader component
     *created main screen component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputComponent } from 'src/shared/components/input/input.component';
+import { FileLoader } from 'src/shared/components/file-loader/file-loader.component';
+import { MainScreenComponent } from 'src/screens/main-screen/main-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    FileLoader,
+    MainScreenComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
