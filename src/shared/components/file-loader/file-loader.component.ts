0b7f36c7664f99ec313a2272b39876f56717c09c
ginoLanetti <40967262+ginoLanetti@users.noreply.github.com>
2019-12-04 09:00:01 +0100

    Reactive form creation (#2)
    *created reactive form with simple validation for gathering data neccessary for chart creation (#2)
    *created reusable alert boxes with different mesages depending on the type of validation error
    *created file loader accepting specific type extensions, using JS FileReader()

--- src/shared/components/file-loader/file-loader.component.ts
+++ src/shared/components/file-loader/file-loader.component.ts
@@ -1,10 +1,24 @@
 import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
     selector: 'app-file-loader',
     templateUrl: './file-loader.component.html',
     styleUrls: ['./file-loader.component.scss']
 })
-export class FileLoader {
-    @Input() labelName:string
+export class FileLoaderComponent {
+    @Input() labelName: string;
+    @Input() formController: FormControl;
+    @Input() accept: string;
+    file: File;
+
+    onFileChange(event): void {
+        this.file = event.target.files[0];
+        const fileReader = new FileReader();
+        fileReader.readAsText(this.file);
+        fileReader.onload = () => {
+            console.log(fileReader.result);
+            this.formController.setValue(fileReader.result)
+        };
+    }
 }
--- src/shared/components/input/input.component.ts
+++ src/shared/components/input/input.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 
 @Component({
@@ -8,7 +9,10 @@ import { Component, Input } from '@angular/core';
 })
 
 export class InputComponent {
-    @Input() labelName: string
-    @Input() inputType: string 
-    
+    @Input() labelName: string;
+    @Input() inputType: string;
+    @Input() formController: FormControl;
+    @Input() errors: any;
+    @Input() ifSubmitted: boolean;
+
 }
